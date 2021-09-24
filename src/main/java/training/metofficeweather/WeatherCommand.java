669e1f965887039a4052008eb36ad930db60bb76
Max Stanford-Taylor <Max.StanfordTaylor@aexp.com>
2021-09-24 11:42:26 +0100

    update error resiliance for weather command

--- src/main/java/training/metofficeweather/WeatherCommand.java
+++ src/main/java/training/metofficeweather/WeatherCommand.java
@@ -14,7 +14,14 @@ import java.util.Locale;
 public class WeatherCommand implements Command {
 	@Override
 	public void Execute(String input, HashMap<String, Locations> locationsHashMap) {
-		String locationId = locationsHashMap.get(input.toUpperCase(Locale.ROOT)).id;
+		String locationId = null;
+		try {
+			locationId = locationsHashMap.get(input.toUpperCase(Locale.ROOT)).id;
+		}
+		catch (Exception e) {
+			System.out.printf("Could not find %s\n", input);
+			return;
+		}
 		try {
 			URL url = new URL("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/"+locationId+"?res=3hourly&key=" + System.getenv("API_KEY"));
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
