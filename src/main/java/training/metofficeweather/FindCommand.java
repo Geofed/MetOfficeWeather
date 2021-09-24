db7058664662a9add76953ae559086e214558b66
vnguy4 <vanthoai.nguyen@aexp.com>
2021-09-24 12:10:31 +0100

    added searchname function from working before FIND method actually work

--- src/main/java/training/metofficeweather/FindCommand.java
+++ src/main/java/training/metofficeweather/FindCommand.java
@@ -3,15 +3,20 @@ package training.metofficeweather;
 import training.metofficeweather.data.Locations;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.stream.Stream;
 
 public class FindCommand implements Command{
 	@Override
 	public void Execute(String input, HashMap<String, Locations> locationsHashMap) {
-		Stream<String> keyStream = locationsHashMap.keySet().stream();
-		boolean match = keyStream.anyMatch(e -> e.equals(input));
+		boolean match = locationsHashMap.keySet().stream().anyMatch(e -> e.equals(input.toUpperCase(Locale.ROOT)));
 		if (match) {
-			keyStream.filter(e -> e.equals(input)).forEach(System.out::println);
+			System.out.println("Found a match");
+			locationsHashMap
+					.keySet()
+					.stream()
+					.filter(e -> e.toUpperCase(Locale.ROOT).equals(input.toUpperCase(Locale.ROOT)))
+					.forEach(e -> System.out.println(locationsHashMap.get(e)));
 		}
 	}
 }
--- src/main/java/training/metofficeweather/WeatherCommand.java
+++ src/main/java/training/metofficeweather/WeatherCommand.java
@@ -3,23 +3,33 @@ package training.metofficeweather;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import training.metofficeweather.data.Locations;
 import training.metofficeweather.data.LocationsRoot;
+import training.metofficeweather.data.WeatherRoot;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class WeatherCommand implements Command {
 	@Override
 	public void Execute(String input, HashMap<String, Locations> locationsHashMap) {
-		String locationId = locationsHashMap.get(input).id;
+		String locationId = null;
+		try {
+			locationId = locationsHashMap.get(input.toUpperCase(Locale.ROOT)).id;
+		}
+		catch (Exception e) {
+			System.out.printf("Could not find %s\n", input);
+			return;
+		}
 		try {
 			URL url = new URL("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/"+locationId+"?res=3hourly&key=" + System.getenv("API_KEY"));
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestProperty("accept", "application/json");
 			InputStream responseStream = connection.getInputStream();
 			ObjectMapper mapper = new ObjectMapper();
-			Object out = mapper.readValue(responseStream, LocationsRoot.class);
+			Object out = mapper.readValue(responseStream, Object.class);
+			System.out.println(out);
 		}
 		catch (Exception e) {
 			System.out.println("Unable to process web request");
