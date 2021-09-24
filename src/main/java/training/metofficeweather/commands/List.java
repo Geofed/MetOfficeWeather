dd2b2302ed5ce2bac1363b06f5540803239270fe
vnguy4 <vanthoai.nguyen@aexp.com>
2021-09-24 12:29:17 +0100

    added id next to locations in list all method

--- src/main/java/training/metofficeweather/commands/List.java
+++ src/main/java/training/metofficeweather/commands/List.java
@@ -8,6 +8,6 @@ import java.util.HashMap;
 public class List implements Command {
 	@Override
 	public void Execute(String input, HashMap<String, Locations> locationsHashMap) {
-		locationsHashMap.keySet().forEach(System.out::println);
+		locationsHashMap.keySet().forEach(e -> System.out.println(e + ",ID:" + locationsHashMap.get(e).id));
 	}
 }
